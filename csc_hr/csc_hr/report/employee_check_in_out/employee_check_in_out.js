1ca954ed1a52318f968f0fd25edec5379c979040
pankajambi <pankaj.wankhede@ambibuzz.com>
2023-03-14 20:05:52 +0800

    repository initiated

---
+++ csc_hr/csc_hr/report/employee_check_in_out/employee_check_in_out.js
@@ -0,0 +1,28 @@
+// Copyright (c) 2023, ithead@ambibuzz.com and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Employee Check In Out"] = {
+	"filters": [
+		{
+			"fieldname":"employee",
+			"label": "Employee",
+			"fieldtype": "Link",
+			"options": "Employee",
+			"reqd": 1
+		},
+		{
+			"fieldname":"start_date",
+			"label": "Start Date",
+			"fieldtype": "Date",
+			"reqd": 1
+		},
+		{
+			"fieldname":"end_date",
+			"label": "End Date",
+			"fieldtype": "Date",
+			"reqd": 1
+		}
+
+	]
+};
