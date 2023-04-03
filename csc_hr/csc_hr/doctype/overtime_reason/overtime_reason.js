73d8df318fe0e4631bcfa5e75c90e5f1fadb9ecf
pankajambi <pankaj.wankhede@ambibuzz.com>
2023-04-03 15:01:51 +0800

    Add Overtime Reason doctype
    Signed-off-by: pankajambi <pankaj.wankhede@ambibuzz.com>

---
+++ csc_hr/csc_hr/doctype/overtime_reason/overtime_reason.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, ithead@ambibuzz.com and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Overtime Reason', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ csc_hr/csc_hr/doctype/overtime_reason_list/overtime_reason_list.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2023, ithead@ambibuzz.com and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+class OvertimeReasonList(Document):
+	pass
